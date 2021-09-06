df5f65a64d7af6b7abcc783034b64a7592e808cd
wangsimiao <wangsimiao.tjpu@bytedance.com>
2021-09-06 09:59:07 +0800

    feat: blog -> 再出发

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,13 +71,13 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+            "https://github.com/Geronimomiao/my-blog-new/blob/main",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+            "https://github.com/Geronimomiao/my-blog-new/blob/main/blog",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
